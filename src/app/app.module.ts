f0e0d9ff6e8a4229030013771577c919bed3b530
quyetndph27146 <quyetndph27146@fpt.edu.vn>
2023-06-23 15:06:58 +0700

    pipe

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,17 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
+import { OnSalePipe } from './on-sale.pipe';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    HomeComponent,
-    AboutComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, HomeComponent, AboutComponent, OnSalePipe],
+  imports: [BrowserModule, AppRoutingModule, FormsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -8,7 +8,18 @@ import { Component, OnInit } from '@angular/core';
 export class HomeComponent implements OnInit {
   name = 'QuyeTrang';
   public age = 21;
+  public traiCay = ['Dâu Tây', 'Bơ', 'Sầu riêng'];
+  public hoaQua = [
+    { ten: 'Dâu Tây', price: 21, haGia: true },
+    { ten: 'Bơ', price: 22, haGia: false },
+    { ten: 'Sầu riêng', price: 12, haGia: true },
+    { ten: 'Dưa hấu', price: -3, haGia: false },
+  ];
   constructor() {}
 
   ngOnInit(): void {}
+
+  public resetName(): void {
+    this.age = 21;
+  }
 }
